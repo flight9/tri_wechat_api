1bb61a6b741b292ee7ea86131837857cd591dd68
flight9 <ealeaf@sohu.com>
2017-08-10 17:01:09 +0800

    change CONST by test account or formal account

--- wechat_account_test.php
+++ wechat_account_test.php
@@ -1,6 +1,17 @@
 <?php
 
-const WECHAT_APPID = 'wx97eaa4a1320ff795';
-const WECHAT_APPSECRET = '4f57a649e1e08de0a8fa1075f94f9cb0';
-const WECHAT_TOKEN = '';
+$use_test_account = true;
+
+define( 'WECHAT_APPID', $use_test_account? 'wx97eaa4a1320ff795': 'wxa4ddad61df39d191');
+define( 'WECHAT_APPSECRET', $use_test_account? '4f57a649e1e08de0a8fa1075f94f9cb0': '298befb9e2bd11279f8af392390803dd');
+define( 'WECHAT_TOKEN', $use_test_account? '': '');
+
+define( 'WECHAT_DOMAIN', $use_test_account? 'http://kyxg4z.natappfree.cc': 'https://app.trifocus.com');
+define( 'WECHAT_TEMPL_ID_APPROVAL', $use_test_account? 'cLsTPx-bp2QxOI8RZGorZjAhHSf-INaoYkeekufh75w': 'XbX2NvV-D5j6AhED5Pu0lYVFBJXXTnJGZwaTdbTv-S4');
+
+// 路由路径表
+$wccfg_state_path = array(
+	'home' => '/',
+	'test' => '/test',
+);
 
