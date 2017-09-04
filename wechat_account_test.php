cb41c415b97ed6f72282a26f28a13f1cf1a40a3a
flight9 <ealeaf@sohu.com>
2017-09-04 15:24:48 +0800

    update url

--- wechat_account_test.php
+++ wechat_account_test.php
@@ -6,7 +6,7 @@ define( 'WECHAT_APPID', $use_test_account? 'wx97eaa4a1320ff795': 'wxa4ddad61df39
 define( 'WECHAT_APPSECRET', $use_test_account? '4f57a649e1e08de0a8fa1075f94f9cb0': '298befb9e2bd11279f8af392390803dd');
 define( 'WECHAT_TOKEN', $use_test_account? '': '');
 
-define( 'WECHAT_DOMAIN', $use_test_account? 'http://m5giuy.natappfree.cc': 'https://app.trifocus.com');
+define( 'WECHAT_DOMAIN', $use_test_account? 'http://tk3wgw.natappfree.cc': 'https://app.trifocus.com');
 define( 'WECHAT_TEMPL_ID_APPROVAL', $use_test_account? 'cLsTPx-bp2QxOI8RZGorZjAhHSf-INaoYkeekufh75w': 'XbX2NvV-D5j6AhED5Pu0lYVFBJXXTnJGZwaTdbTv-S4');
 
 // 路由路径表
